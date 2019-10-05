fa45605a2f9c76e4bb21c0585ad16f2c5c817763
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-05 11:16:27 +0200

    Add project files.

---
+++ ConversioneTraBasi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConversioneTraBasi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
